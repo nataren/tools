790eb1369807fdd5b72eda1559bbcf3db69cc549
César López-Natarén <cesar@nataren.io>
2017-08-14 15:46:05 -0400

    Add arguments parsing, and logging

--- zim-concatenator.py
+++ zim-concatenator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 from os.path import expanduser
 from os.path import exists
 from os.path import isdir
@@ -11,9 +13,13 @@ from re import search
 from re import compile
 
 import subprocess
+import argparse
+import logging
 
 class Concatenator(object):
-    def __init__(self, path, matching_regex_exp=r'(?P<prefix>\w+\.zim)(?P<suffix>\w+)'):
+    def __init__(self, path, logger, matching_regex_exp=r'(?P<prefix>\w+\.zim)(?P<suffix>\w+)'):
+        self.logger = logger
+        self.logger.debug('init concatenator')
         self.path = path
         self.resolved_path = abspath(expanduser(self.path))
         if not exists(self.resolved_path) or not isdir(self.resolved_path):
@@ -43,16 +49,38 @@ class Concatenator(object):
             already_there.append(join(dirname, prefix + suffix))
 
     def concatenate(self):
+        self.logger.debug('concatenate')
         for new_file, filenames in self.groups_by_prefix.items():
             filenames.sort()
-            cmd = 'cat {}'.format(' '.join(filenames))
+            file_names_args = ' '.join(filenames)
+            cmd = 'cat {}'.format(file_names_args)
+            self.logger.debug('Going to create `%s` as a concatenation of `%s`', new_file, file_names_args)
             with open(new_file, 'wb') as f:
                 f.write(subprocess.check_output([cmd], shell=True))
 
     def run(self):
+        self.logger.debug('run')
         walk(self.resolved_path, self.group_by_prefix, '')
         self.concatenate()
 
 if __name__ == '__main__':
-    concatenator = Concatenator(path='.')
+    parser = argparse.ArgumentParser(description='Concatenate several `zim` files.')
+    parser.add_argument('--path', '-path', required=True, dest='path', help='The path of the directory that will be used to recursively search for `zim` files that are chunked')
+    parser.add_argument('--verbose', '-v', action='count')
+    args = parser.parse_args()
+
+    logger = logging.getLogger('zim-concatenator')
+    if args.verbose == 1:
+        logger.setLevel(logging.INFO)
+    elif args.verbose == 2:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARN)
+    h = logging.StreamHandler(sys.stdout)
+    h.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    h.setFormatter(formatter)
+    logger.addHandler(h)
+
+    concatenator = Concatenator(path=args.path, logger=logger)
     concatenator.run()
